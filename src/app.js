b378d1e83761decb52956cf6c64914cdc7c13bd3
gabriel <gabriel.m.ledesma96@gmail.com>
2024-01-05 15:19:10 -0300

    netrlify

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import 'dotenv/config';
 
 
 const url = process.env.URL_MONGO
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT
 
 const app = express();
 //permiso para las rutas
